7968269e688ad300daced98254957194fef2a43a
KavinR34 <kavinkavin2209@gmail.com>
2025-05-01 13:30:43 +0530

    Fix static files setup

--- nsp_web/settings.py
+++ nsp_web/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'nsp_web.urls'
@@ -117,10 +119,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-BASE_DIR/'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 
